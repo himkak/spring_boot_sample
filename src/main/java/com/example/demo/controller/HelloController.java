d397ab8efc794b9721976ec7e3d993133fb80dbf
Himanshu KAKAR <himanshukakar@dbs.com>
2018-07-07 01:13:06 +0530

    added src

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(method=RequestMethod.GET,path="/hello")
+	public String hello() {
+		System.out.println("Inside hello method.");
+		return "Hello World";
+	}
+}
